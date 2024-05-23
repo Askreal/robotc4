97912d526d87324b1352f7b28684199e06eb1589
LackSweet <86765808+Askreal@users.noreply.github.com>
2024-05-23 14:26:09 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def right():
         """)
     sensors.dd_mmotor(AnalogPin.P13, 0, AnalogPin.P14, 220)
     sensors.dd_mmotor(AnalogPin.P15, 1, AnalogPin.P16, 200)
-
 def Forward():
     basic.show_leds("""
         . . # . .
@@ -19,7 +18,9 @@ def Forward():
         """)
     sensors.dd_mmotor(AnalogPin.P13, 0, AnalogPin.P14, 255)
     sensors.dd_mmotor(AnalogPin.P15, 0, AnalogPin.P16, 255)
-
+def UnCatch():
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.servo_write_pin(AnalogPin.P12, 180)
 def left():
     basic.show_leds("""
         . . . . .
@@ -30,25 +31,6 @@ def left():
         """)
     sensors.dd_mmotor(AnalogPin.P13, 1, AnalogPin.P14, 220)
     sensors.dd_mmotor(AnalogPin.P15, 0, AnalogPin.P16, 220)
-
-def Backward():
-    basic.show_leds("""
-        . . # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        . . # . .
-        """)
-
-def Catch():
-    pins.digital_write_pin(DigitalPin.P12, 0)
-    pins.servo_write_pin(AnalogPin.P12, 0)
-
-def UnCatch():
-    pins.digital_write_pin(DigitalPin.P12, 0)
-    pins.servo_write_pin(AnalogPin.P12, 180)
-
-
 def stop():
     basic.show_leds("""
         . . . . .
@@ -59,9 +41,74 @@ def stop():
         """)
     sensors.dd_mmotor(AnalogPin.P13, 1, AnalogPin.P14, 0)
     sensors.dd_mmotor(AnalogPin.P15, 0, AnalogPin.P16, 0)
-
 def Stop2():
     pins.digital_write_pin(DigitalPin.P12, 0)
     pins.servo_write_pin(AnalogPin.P12, 90)
+def Catch():
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.servo_write_pin(AnalogPin.P12, 0)
+def Backward():
+    basic.show_leds("""
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        . . # . .
+        """)
 
+def diver():
+    if (pins.digital_read_pin(DigitalPin.P1) == 0 and pins.digital_read_pin(DigitalPin.P8) == 0):
+        forward()
+    elif (pins.digital_read_pin(DigitalPin.P1) == 1 and pins.digital_read_pin(DigitalPin.P8) == 0):
+        while (pins.digital_read_pin(DigitalPin.P1) == 1):
+            left()
+    elif (pins.digital_read_pin(DigitalPin.P1) == 0 and pins.digital_read_pin(DigitalPin.P8) == 1):
+        while (pins.digital_read_pin(DigitalPin.P8) == 1):
+            right()
+    elif (pins.digital_read_pin(DigitalPin.P1) == 1 and pins.digital_read_pin(DigitalPin.P8) == 1):
+        pass
+    else : 
+    basic.show_leds("""
+    . # . . .
+    . . # . .
+    . . # . .
+    . . . # .
+    . . . # .
+    """)
+    return 0
+
+def NLeft():
+    stop()
+    pause(300)
+    forward()
+    pause(200)
+    while pins.digital_read_pin(DigitalPin.P1) == 0:
+        left()
+    stop()
+    pause(100)
+    while pins.digital_read_pin(DigitalPin.P1) == 1:
+        left()
+    stop()
+    pause(100)
+    return 0
+
+def NRight():
+    stop()
+    pause(300)
+    while pins.digital_read_pin(DigitalPin.P1) == 0:
+        right()
+    stop()
+    pause(100)
+    while pins.digital_read_pin(DigitalPin.P1) == 1:
+        right()
+    stop()
+    pause(100)
+    return 0
+
+
+
+def event(num):
+    if num == 1:
+        NLeft()
+    else basic.show_string("No event left")
     
--- main.ts
+++ main.ts
@@ -1,3 +1,105 @@
-basic.forever(function on_forever() {
-    
-})
+function right () {
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . . . .
+        `)
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    220
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    1,
+    AnalogPin.P16,
+    200
+    )
+}
+function Forward () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    255
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    255
+    )
+}
+function UnCatch () {
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.servoWritePin(AnalogPin.P12, 180)
+}
+function left () {
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . . . .
+        `)
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    1,
+    AnalogPin.P14,
+    220
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    220
+    )
+}
+function stop () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    1,
+    AnalogPin.P14,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    0
+    )
+}
+function Stop2 () {
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.servoWritePin(AnalogPin.P12, 90)
+}
+function Catch () {
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.servoWritePin(AnalogPin.P12, 0)
+}
+function Backward () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        . . # . .
+        `)
+}
